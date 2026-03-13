72b2c7de82aec196a5b5d5b399230ef3bfb316cd
roasted-oolong <65867683+roasted-oolong@users.noreply.github.com>
2026-03-12 22:22:07 -0500

    Milestone 5 Migrations: organizations, teams, series, memberships, novels, pgvector

---
+++ db/migrate/20260312000003_create_series.rb
@@ -0,0 +1,10 @@
+class CreateSeries < ActiveRecord::Migration[8.1]
+  def change
+    create_table :series do |t|
+      t.references :organization, null: false, foreign_key: true
+      t.string     :name,         null: false
+
+      t.timestamps
+    end
+  end
+end
